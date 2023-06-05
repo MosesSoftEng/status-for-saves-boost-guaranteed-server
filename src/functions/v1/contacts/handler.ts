67de5fc898287120b2ab6b9e222d449b1e3ca781
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-05 11:53:39 +0300

    Add deleteContact function
    Adds a new function deleteContact to handle the deletion of a contact based on the user and phone number. The function receives an APIGatewayProxyEvent, parses the user and phone values from the request body, and calls the deleteContactByUserAndPhone function. It returns a success response with a message when the contact is successfully deleted, and an error response if the deletion fails.

--- src/functions/v1/contacts/handler.ts
+++ src/functions/v1/contacts/handler.ts
@@ -1,8 +1,8 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
 import Contact from './Contact';
-import {createContact} from './ContactsRepository';
+import {createContact, deleteContactByUserAndPhone} from './ContactsRepository';
 
-
+// TODO: Add jsdocs and lof error message to console.
 export const saveContact = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 	const {user, phone} = JSON.parse(event.body);
 
@@ -32,6 +32,41 @@ export const saveContact = async (event: APIGatewayProxyEvent): Promise<APIGatew
 			},
 			body: JSON.stringify({
 				message,
+			}),
+		};
+	}
+};
+
+/**
+ * Deletes a contact based on the user and phone number.
+ * @param {APIGatewayProxyEvent} event - The API Gateway event object.
+ * @returns {Promise<APIGatewayProxyResult>} A promise that resolves to the API Gateway proxy result.
+ */
+export const deleteContact = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	const {user, phone} = JSON.parse(event.body);
+
+	try {
+		await deleteContactByUserAndPhone(Number(user), Number(phone));
+
+		return {
+			statusCode: 200,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
+			body: JSON.stringify({
+				message: 'Contact deleted',
+			}),
+		};
+	} catch (error) {
+		console.error('Error deleting contact:', error);
+
+		return {
+			statusCode: 500,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
+			body: JSON.stringify({
+				message: 'Failed to delete contact',
 			}),
 		};
 	}
