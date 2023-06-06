70b37ab4577f63c4ab6a07b52aded39457adf554
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-06 17:33:35 +0300

    Add getUsersSavedUser function
    Adds the getUsersSavedUser function to handle the retrieval of users that have saved a specific phone number. The function takes in an API Gateway event and uses the getUsersThatSavePhoneNumber function from the ContactsRepository to retrieve the users.

--- src/functions/v1/users/handler.ts
+++ src/functions/v1/users/handler.ts
@@ -1,6 +1,7 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
 import User from './User';
 import {getUsers} from './UsersRepository';
+import {getUsersThatSavePhoneNumber} from '../contacts/ContactsRepository';
 
 /**
  * Lambda function to handle API Gateway event and retrieve users.
@@ -26,6 +27,45 @@ export const users = async (event: APIGatewayProxyEvent): Promise<APIGatewayProx
 		};
 	} catch (error) {
 		console.error('Error retrieving users:', error);
+
+		return {
+			statusCode: 500,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
+			body: JSON.stringify({
+				message: 'Failed to retrieve users',
+				error: error.message,
+			}),
+		};
+	}
+};
+
+
+/**
+ * Retrieves users that have saved a specific phone number.
+ * @param {APIGatewayProxyEvent} event - The event object representing the HTTP request.
+ * @returns {Promise<APIGatewayProxyResult>} - A promise that resolves to the APIGatewayProxyResult object.
+ */
+export const getUsersSavedUser = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	const {user} = event.pathParameters;
+	const {lastIndex, limit} = event.queryStringParameters ?? {};
+
+	try {
+		const users = await getUsersThatSavePhoneNumber(Number(user), Number(lastIndex), Number(limit));
+
+		return {
+			statusCode: 200,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
+			body: JSON.stringify({
+				message: 'Users retrieved successfully',
+				data: users,
+			}),
+		};
+	} catch (error) {
+		console.error('getUsersSavedUser, error: ', error);
 
 		return {
 			statusCode: 500,
