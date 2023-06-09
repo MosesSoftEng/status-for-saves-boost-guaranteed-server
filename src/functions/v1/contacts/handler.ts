0a397c7c73ac13cc27cdd74c98e147e38d5219b4
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-09 15:49:06 +0300

    Add code to send FCM noti

--- src/functions/v1/contacts/handler.ts
+++ src/functions/v1/contacts/handler.ts
@@ -1,6 +1,8 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
 import Contact from './Contact';
 import {createContact, deleteContactByUserAndPhone} from './ContactsRepository';
+import {getUserFCMToken, sendFCMNotification} from '../fcm-token/FCMTokenRepo';
+import FCMToken from '../fcm-token/FCMToken';
 
 // TODO: Add jsdocs and lof error message to console.
 export const saveContact = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -15,6 +17,12 @@ export const saveContact = async (event: APIGatewayProxyEvent): Promise<APIGatew
 	try {
 		await createContact(contact);
 
+		const fcmToken: FCMToken = await getUserFCMToken(Number(phone));
+
+		if(fcmToken)
+			// Send notification to contact.
+			await sendFCMNotification(fcmToken.token, {title: 'New contact save!', body: 'A user has saved your contact, save back to retain save.'});
+
 		return {
 			statusCode: 200,
 			headers: {
@@ -22,6 +30,7 @@ export const saveContact = async (event: APIGatewayProxyEvent): Promise<APIGatew
 			},
 			body: JSON.stringify({
 				message: 'Contact save',
+				data: fcmToken
 			}),
 		};
 	} catch ({message}) {
