5d4277a2c9c1e34685a31fe0d3b6e0bc0a7d0648
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-09 15:47:55 +0300

    Add function to send FCM notification

--- src/functions/v1/fcm-token/FCMTokenRepo.ts
+++ src/functions/v1/fcm-token/FCMTokenRepo.ts
@@ -1,9 +1,15 @@
 import * as AWS from 'aws-sdk';
 import FCMToken from './FCMToken';
+import * as admin from 'firebase-admin';
+import * as serviceAccount from '../../../status-for-saves-firebase-adminsdk-x6dla-bad19d9573.json';
+import {Notification} from 'firebase-admin/lib/messaging/messaging-api';
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 const TABLE_FCM_TOKEN = process.env.TABLE_FCM_TOKEN;
 
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+});
 
 /**
  * Creates a new FCM token in the DynamoDB table.
@@ -18,3 +24,21 @@ export const createFCMToken = async (fcmToken: FCMToken): Promise<FCMToken> => {
 
 	return fcmToken;
 };
+
+
+/**
+ * Sends a Firebase Cloud Messaging (FCM) notification to the specified token.
+ * @param {string} token - The FCM token of the recipient device.
+ * @param {Notification} notification - The notification object containing the title and body.
+ * @returns {Promise<void>} - A promise that resolves when the notification is sent successfully.
+ */
+export const sendFCMNotification = async (token: string, notification: Notification ): Promise<void> => {
+	const message = {
+		token,
+		notification,
+	};
+
+	await admin.messaging().send(message);
+};
+
+
