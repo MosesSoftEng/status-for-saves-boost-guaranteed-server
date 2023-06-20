b7aa958e637abfd7b70152b74a6273fab7b28bb0
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-20 23:27:25 +0300

    Add interface docs

--- src/functions/v1/contacts/Contact.ts
+++ src/functions/v1/contacts/Contact.ts
@@ -1,4 +1,15 @@
 // TODO: set defaults
+
+/**
+ * The `Contact` interface represents a contact in a contact list.
+ *
+ * @interface
+ * @extends {Object}
+ *
+ * @property {number} user The user phone saving the contact.
+ * @property {number} phone The phone number of the contact.
+ * @property {number} date The date the contact was created.
+ */
 export default interface Contact {
 	user: number;
 	phone: number;
