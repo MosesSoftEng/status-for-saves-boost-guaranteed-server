708b8f5fe1a996a99c452bb980b4e44b305b3701
Moses Mwangi <moses.soft.eng@gmail.com>
2023-07-04 18:56:55 +0300

    Remove unused functions

--- src/functions/v1/contacts/ContactsRepository.ts
+++ src/functions/v1/contacts/ContactsRepository.ts
@@ -1,6 +1,5 @@
 import * as AWS from 'aws-sdk';
 import Contact from './Contact';
-import User from '../users/User';
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 const TABLE_CONTACTS = process.env.TABLE_CONTACTS;
@@ -109,67 +108,4 @@ export const deleteContactByUserAndPhone = async (user: number, phone: number): 
 	};
 
 	await docClient.delete(params).promise();
-};
-
-/**
- * Retrieves a list of users who have saved a particular phone number.
- *
- * @param {number} phone The phone number of the users to retrieve.
- * @param {number} lastIndex The index of the last user that was retrieved in the previous call to the function.
- * @param {number} limit The limit on the number of users to retrieve.
- * @returns {Promise<User[]>} A promise that resolves to a list of User objects.
- */
-// TODO: Move function to user repository.
-export const getUsersThatSavePhoneNumber = async (phone: number, lastIndex = 0, limit = 10): Promise<User[]> => {
-	const params = {
-		TableName: TABLE_CONTACTS,
-		KeyConditionExpression: '#user = :userValue AND #phone > :lastIndexValue',
-		FilterExpression: '#isUserContactOfPhone = :isUserContactOfPhoneValue',
-		ExpressionAttributeNames: {
-			'#user': 'user',
-			'#phone': 'phone',
-			'#isUserContactOfPhone': 'isUserContactOfPhone',
-		},
-		ExpressionAttributeValues: {
-			':userValue': phone,
-			':lastIndexValue': lastIndex,
-			':isUserContactOfPhoneValue': false,
-		},
-		Limit: limit,
-	};
-
-	const result = await docClient.query(params).promise();
-
-	const users: User[] = result.Items.map((contact) => {
-		const {phone, date} = contact;
-		return {phone, date};
-	});
-
-	return users;
-};
-
-// TODO: Delete function f not in use.
-export const getUsersAreContact = async (phone: number, lastIndex = 0, limit = undefined): Promise<User[]> => {
-	const params = {
-		TableName: TABLE_CONTACTS,
-		FilterExpression: '#user = :phoneValue AND #phone <> :phoneValue',
-		ProjectionExpression: '#phone, #date',
-		ExpressionAttributeNames: {
-			'#user': 'user',
-			'#phone': 'phone',
-			'#date': 'date',
-		},
-		ExpressionAttributeValues: {
-			':phoneValue': phone,
-		},
-		ExclusiveStartKey: lastIndex ? {user: lastIndex} : undefined,
-	};
-
-	if (limit) {
-		params.Limit = limit;
-	}
-
-	const result = await docClient.scan(params).promise();
-
-	return result.Items as User[];
 };
